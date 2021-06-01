78496630f98651b43128db9f4ff50ab112ddd0e0
Merzouk-ilyes <i.merzouk@esi-sba.dz>
2021-06-01 01:12:13 +0100

    some modifications

--- views.js
+++ views.js
@@ -36,3 +36,41 @@ function hidePopup() {
    else
        document.getElementById('circle').style.opacity = .3; 
 });
+
+  
+const eye = document.querySelector('#eye');
+const eye2 = document.querySelector('#eye2');
+const password = document.querySelector('#password');
+const confirmer = document.querySelector('#confirmer');
+eye.addEventListener('click', (e) =>{
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    if(type==='password'){
+        eye.classList.remove('fi-rr-eye-crossed');
+        eye.classList.toggle('fi-rr-eye');
+    }else{
+        eye.classList.remove('fi-rr-eye');
+        eye.classList.toggle('fi-rr-eye-crossed');
+        
+    }
+     
+   
+});
+eye2.addEventListener('click', (e) =>{
+    // toggle the type attribute
+    const type = confirmer.getAttribute('type') === 'password' ? 'text' : 'password';
+    confirmer.setAttribute('type', type);
+    // toggle the eye slash icon
+    if(type==='password'){
+        eye2.classList.remove('fi-rr-eye-crossed');
+        eye2.classList.toggle('fi-rr-eye');
+    }else{
+        eye2.classList.remove('fi-rr-eye');
+        eye2.classList.toggle('fi-rr-eye-crossed');
+        
+    }
+     
+   
+});
